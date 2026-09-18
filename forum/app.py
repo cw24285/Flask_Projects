9597b0a8de58253b8f200f03ad1ccf68685bf38e
Corban <cw24285@my.westlake.school.nz>
2026-09-18 12:49:29 +1200

    asds

--- forum/app.py
+++ forum/app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import sqlite3
 
 app = Flask (__name__)
 
+DB = "Flask_Projects/forum/forum.db"
+
+def init_db():
+    conn = sqlite3.connect(DB)
+    conn.execute("""CREATE TABLE IF NOT EXISTS posts (
+                        post_id INTEGER PRIMARY KEY,
+                        author TEXT NOT NULL,
+                        message TEXT NOT NULL)""")
+    conn.commit()
+    conn.close()
+
+init_db()
+
+def get_db():
+    conn = sqlite3.connect(DB)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route("/forum")
 def forum():
-    return render_template("forum.html")
+    conn = get_db()
+    posts = conn.execute("SELECT * FROM posts ORDER BY post_id DESC").fetchall()
+    conn.close()
+    return render_template("forum.html", posts = posts)
+
+@app.route("/")
+def redirect_to_forum():
+    return redirect("/forum")
 
 if __name__ == "__main__":
     app.run(debug=True)
