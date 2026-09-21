04d6b768f3e88e3bae1fa9d5f084ce619bec106a
Corban <cw24285@my.westlake.school.nz>
2026-09-21 12:00:16 +1200

    asd

--- forum/app.py
+++ forum/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 import sqlite3
 
 app = Flask (__name__)
@@ -27,6 +27,12 @@ def forum():
     posts = conn.execute("SELECT * FROM posts ORDER BY post_id DESC").fetchall()
     conn.close()
     return render_template("forum.html", posts = posts)
+
+@app.route("/post", methods=["POST"]) 
+def new_post(): 
+    author = request.form["author"] 
+    message = request.form["message"] 
+    return f"Got a post from {author}: {message}" 
 
 @app.route("/")
 def redirect_to_forum():
